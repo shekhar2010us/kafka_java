ab1e3498b29091a584967514ebcd43d56ceb0d88
shekhar_a_agrawal <shekhar.a.agrawal@oracle.com>
2020-02-19 17:26:23 -0600

    msg

--- src/main/java/com/shekhar/kafka/basic/ConsumerDemo.java
+++ src/main/java/com/shekhar/kafka/basic/ConsumerDemo.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +19,7 @@ public class ConsumerDemo {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
-        String hostUrl = "localhost:9092";
+        String hostUrl = "127.0.0.1:9092";
         String topic = "first_topic";
         String groupId = "my-app-2";
 
@@ -28,8 +29,8 @@ public class ConsumerDemo {
         // check: https://kafka.apache.org/documentation/#consumerconfigs
         Properties props = new Properties();
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, hostUrl);
-        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, reset_offsets.get(0));
 
