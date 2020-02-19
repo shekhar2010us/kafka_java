0e1562f266f7d344f18124a6b0707f04fdc729e2
shekhar_a_agrawal <shekhar.a.agrawal@oracle.com>
2020-02-19 16:31:25 -0600

    msg

--- src/main/java/com/shekhar/kafka/basic/ProducerDemoWithCallback.java
+++ src/main/java/com/shekhar/kafka/basic/ProducerDemoWithCallback.java
@@ -32,22 +32,24 @@ public class ProducerDemoWithCallback {
 
 
         // send data - async
-        ProducerRecord record = new ProducerRecord<String, String>(topic, "hello world callback");
-        producer.send(record, new Callback() {
-            @Override
-            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                // executes every time when a record is sent successfully or an exception is thrown
-                if (e == null) {
-                    logger.info("received new metadata: \n\t" +
-                            "Topic: " + recordMetadata.topic() + "\n\t" +
-                            "Offset: " + recordMetadata.offset() + "\n\t" +
-                            "Partition: " + recordMetadata.partition() + "\n\t" +
-                            "Timestamp: " + recordMetadata.timestamp() + "\n");
-                } else {
-                    logger.error("Error in Producing", e);
+        for (int i=0; i<10; ++i) {
+            ProducerRecord record = new ProducerRecord<String, String>(topic, "hello world callback - "+i);
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time when a record is sent successfully or an exception is thrown
+                    if (e == null) {
+                        logger.info("received new metadata: \n\t" +
+                                "Topic: " + recordMetadata.topic() + "\n\t" +
+                                "Offset: " + recordMetadata.offset() + "\n\t" +
+                                "Partition: " + recordMetadata.partition() + "\n\t" +
+                                "Timestamp: " + recordMetadata.timestamp() + "\n");
+                    } else {
+                        logger.error("Error in Producing", e);
+                    }
                 }
-            }
-        });
+            });
+        }
 
         // flush and close producer
         producer.close();
